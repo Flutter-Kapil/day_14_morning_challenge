b2734f9b7f6b11810b0e9a76ed7e742440477da8
kapil jhajhria <jhajhria44@gmail.com>
2019-10-18 14:55:09 +0530

    completed

--- bin/main.dart
+++ bin/main.dart
@@ -7,11 +7,29 @@
 // multiplicative(39) ➞ 3
 /// Because 3 * 9 = 27, 2 * 7 = 14, 1 * 4 = 4 and 4 has only one digit.
 int multiplicative(int number) {
-  return null;
+  int count = 0;
+  int temp = 1;
+  for (int i = 0; number.toString().length != 1; i++) {
+    for (int x = 0; x < number.toString().length; x++) {
+      temp = temp * int.parse(number.toString()[x]);
+    }
+    number = temp;
+    temp = 1;
+    count++;
+  }
+  return count;
 }
+
+//var one = int.parse('1')
 
 // Challenge 3
 // Mexican Wave Simulator
+
+//int multiplicative(int number) {
+//
+//  }
+//  return count;
+//}
 /// The wave (known as a Mexican wave in the English-speaking world outside North
 /// America) is an example of metachronal rhythm achieved in a packed stadium when successive groups of spectators briefly stand, yell, and raise their arms.
 //  https://s3.amazonaws.com/edabit-images/mex.gif
@@ -23,7 +41,21 @@ int multiplicative(int number) {
 
 //  wave(" ") ➞ []
 List<String> wave(String word) {
-  return null;
+  int length = word.length;
+  List<String> waveList = [];
+  for (int i = 0; i < length; i++) {
+    if (word[i] != ' ') {
+      String newWord = word.substring(0, i) +
+          word[i].toUpperCase() +
+          word.substring(i + 1, length);
+      waveList.add(newWord);
+    }
+  }
+
+  return waveList;
 }
 
-main() {}
+main() {
+  print(wave("just do it"));
+  print(multiplicative(9));
+}
